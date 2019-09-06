421a9a55000f6f58f68367172a4021814b996e31
Hemant Kurande <hkurande@yahoo.com>
2019-09-06 10:30:00 -0400

    Added random messages to how are you entry point

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 import os
 import json
 from flask import Flask
+def getMessage():
+    import random
+    messages = ["I am one", "I am two", "I am three", "I am four", "I am five"]
+    myNum = random.randint(0, len(messages))
+    myMessage = messages[myNum]
+    return myMessage
 
 app = Flask(__name__)
 
@@ -15,7 +21,7 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about you'
+    return getMessage()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
